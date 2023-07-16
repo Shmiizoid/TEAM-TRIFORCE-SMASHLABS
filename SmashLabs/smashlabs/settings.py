e399ca6f5a3e4f9a689d79ebc74c8a9834bcf831
Shmiizoid <eafuentes949@gmail.com>
2023-07-16 16:56:32 -0700

    db created, home works

--- SmashLabs/smashlabs/settings.py
+++ SmashLabs/smashlabs/settings.py
@@ -76,10 +76,11 @@ WSGI_APPLICATION = 'smashlabs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smashlabs',
     }
 }
+
 
 
 # Password validation
