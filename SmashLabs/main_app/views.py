89ee4c1a109e124589e4f2ab17179f5bbdc624ff
Shmiizoid <eafuentes949@gmail.com>
2023-07-18 16:33:57 -0700

    fixed index

--- SmashLabs/main_app/views.py
+++ SmashLabs/main_app/views.py
@@ -8,54 +8,50 @@ from .forms import StageForm
 
 
 
-# fighters = [
+fighters = [
 
-#     {
-#         'name': 'Mario',
-#         'image': 'mario.png',
-#         'series_name': 'Super Mario',
-#         'fighter_style': 'All-Rounder',
-#         'move_set': 'Fireball, Super Jump Punch, Cape',
-#         'description': 'The iconic plumber from the Mushroom Kingdom.',
-#         'series_id': 1,
-#     },
-#     {
-#         'name': 'Link',
-#         'image': 'link.png',
-#         'series_name': 'The Legend of Zelda',
-#         'fighter_style': 'Swordfighter',
-#         'move_set': "Hero's Bow, Spin Attack, Bomb",
-#         'description': 'The hero clad in green who wields the Master Sword.',
-#         'series_id': 2,
-#     },
-#     {
-#         'name': 'Samus',
-#         'image': 'samus.png',
-#         'series_name': 'Metroid',
-#         'fighter_style': 'Projectile-based',
-#         'move_set': 'Charge Shot, Missile, Screw Attack',
-#         'description': 'The intergalactic bounty hunter in her armored suit.',
-#         'series_id': 3,
-#     },
-#     {
-#         'name': 'Pikachu',
-#         'image': 'pikachu.png',
-#         'series_name': 'Pokémon',
-#         'fighter_style': 'Agile Electric',
-#         'move_set': 'Thunder Jolt, Quick Attack, Thunder',
-#         'description': 'The lovable electric rodent and mascot of Pokémon.',
-#         'series_id': 4,
-#     },
-#     {
-#         'name': 'Donkey Kong',
-#         'image': 'donkey_kong.png',
-#         'series_name': 'Donkey Kong',
-#         'fighter_style': 'Powerful Brawler',
-#         'move_set': 'Giant Punch, Spinning Kong, Headbutt',
-#         'description': 'The king of the jungle with incredible strength.',
-#         'series_id': 5,
-#     },
-# ]
+    {
+        'name': 'Mario',
+        'image': 'mario.png',
+        'series_name': 'Super Mario',
+        'fighter_style': 'All-Rounder',
+        'move_set': 'Fireball, Super Jump Punch, Cape',
+        'description': 'The iconic plumber from the Mushroom Kingdom.',
+    },
+    {
+        'name': 'Link',
+        'image': 'link.png',
+        'series_name': 'The Legend of Zelda',
+        'fighter_style': 'Swordfighter',
+        'move_set': "Hero's Bow, Spin Attack, Bomb",
+        'description': 'The hero clad in green who wields the Master Sword.',
+    },
+    {
+        'name': 'Samus',
+        'image': 'samus.png',
+        'series_name': 'Metroid',
+        'fighter_style': 'Projectile-based',
+        'move_set': 'Charge Shot, Missile, Screw Attack',
+        'description': 'The intergalactic bounty hunter in her armored suit.',
+    },
+    {
+        'name': 'Pikachu',
+        'image': 'pikachu.png',
+        'series_name': 'Pokémon',
+        'fighter_style': 'Agile Electric',
+        'move_set': 'Thunder Jolt, Quick Attack, Thunder',
+        'description': 'The lovable electric rodent and mascot of Pokémon.',
+    },
+    {
+        'name': 'Donkey Kong',
+        'image': 'donkey_kong.png',
+        'series_name': 'Donkey Kong',
+        'fighter_style': 'Powerful Brawler',
+        'move_set': 'Giant Punch, Spinning Kong, Headbutt',
+        'description': 'The king of the jungle with incredible strength.',
+
+    },
+]
 
 # Create your views here.
 def home(request):
@@ -66,7 +62,7 @@ def about(request):
 
 def fighters_index(request):
     return render(request, 'fighters/index.html', {
-       'fighters': Fighter.objects.all()
+       'fighters': fighters
     })
 
 def fighters_detail(request, fighter_id):
