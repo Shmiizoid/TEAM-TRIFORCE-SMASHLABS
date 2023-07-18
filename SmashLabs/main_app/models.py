ea838df7e8a5f5502f0d640152dc15fd6e1dd3f5
omab1789 <omab.1789@icloud.com>
2023-07-18 17:58:34 -0500

    Added stage models

--- SmashLabs/main_app/models.py
+++ SmashLabs/main_app/models.py
@@ -4,15 +4,24 @@ from django.urls import reverse
 class Fighter(models.Model):
     name = models.CharField(max_length=100)
     image = models.CharField(max_length=100)
-    series_name = models.CharField(max_length=100)
     fighter_style = models.CharField(max_length=100)
     move_set = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
-    series_id = models.IntegerField()
+    series_name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'{self.name}, {self.series_name}'
-    
+        return f'{self.name}, Series: {self.series_name}'
+
     def get_absolute_url(self):
         return reverse('detail', kwargs={'fighter_id': self.id})
-    
+
+class Stage(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+    series_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name}, Series: {self.series_name}'
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'stage_id': self.id})
