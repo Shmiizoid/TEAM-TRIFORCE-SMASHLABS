f063bbfc9309e64a1bcf4e0ae257a699e52fa223
annakuz <annaykuzmin@gmail.com>
2023-07-20 13:26:54 -0700

    home page aos

--- SmashLabs/main_app/forms.py
+++ SmashLabs/main_app/forms.py
@@ -5,3 +5,13 @@
 #   class Meta:
 #     model = Stage
 #     fields = '__all__'
+
+
+
+# from django import forms
+# from .models import Fighter
+
+# class FighterForm(forms.ModelForm):
+#     class Meta:
+#         model = Fighter
+#         fields = ('name', 'image', 'fighter_style', 'move_set', 'series_name')
