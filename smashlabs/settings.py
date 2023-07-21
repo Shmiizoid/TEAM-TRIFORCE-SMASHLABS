1402aa576f2cf0cfd01f9b3a7f92b677d3e00e1c
omab1789 <omab.1789@icloud.com>
2023-07-21 14:46:32 -0500

    Config deployment

--- smashlabs/settings.py
+++ smashlabs/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*=^tvz_z0f(uk-$e-!f-u9uq++(zia#_k9f0agp+5q+dq%u=xv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ['MODE'] == 'dev' else False
 
 ALLOWED_HOSTS = []
 
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'smashlabs.urls'
@@ -77,9 +79,14 @@ WSGI_APPLICATION = 'smashlabs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'smashlabs',
+        'NAME': 'neondb',
+        'USER': 'omer-abdul-hameed',
+        'PASSWORD': os.environ['SECRET_KEY'],
+        'HOST': 'ep-calm-leaf-143841.us-west-2.aws.neon.tech',
+        'PORT': '5432',
     }
 }
+
 
 
 
@@ -123,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Other settings above
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
+
