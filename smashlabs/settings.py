d32256c449bee27850da398f391b79d464d7cb81
omab1789 <omab.1789@icloud.com>
2023-07-21 15:49:59 -0500

    justin

--- smashlabs/settings.py
+++ smashlabs/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-*=^tvz_z0f(uk-$e-!f-u9uq++(zia#_k9f0agp+5q+dq%u=xv'
 SECRET_KEY = os.environ['SECRET_KEY']
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ['MODE'] == 'dev' else False
 
