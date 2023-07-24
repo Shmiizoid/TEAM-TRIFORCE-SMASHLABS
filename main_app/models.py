2d3f67c71c038b5ee4177bd87ea5ce8b44a029c4
omab1789 <omab.1789@icloud.com>
2023-07-24 10:29:37 -0500

    css fixes

--- main_app/models.py
+++ main_app/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.urls import reverse
 
 class Fighter(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.CharField(max_length=100)
-    fighter_style = models.CharField(max_length=100)
-    move_set = models.CharField(max_length=100)
+    name = models.TextField(max_length=100)
+    image = models.TextField(max_length=100)
+    fighter_style = models.TextField(max_length=100)
+    move_set = models.TextField(max_length=100)
     description = models.TextField(max_length=250)
-    series_name = models.CharField(max_length=100)
+    series_name = models.TextField(max_length=100)
 
     def __str__(self):
         return f'{self.name}, Series: {self.series_name}'
@@ -16,9 +16,9 @@ class Fighter(models.Model):
         return reverse('detail', kwargs={'fighter_id': self.id})
 
 class Stage(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.CharField(max_length=100)
-    series_name = models.CharField(max_length=100)
+    name = models.TextField(max_length=100)
+    image = models.TextField(max_length=100)
+    series_name = models.TextField(max_length=100)
 
     def __str__(self):
         return f'{self.name}, Series: {self.series_name}'
